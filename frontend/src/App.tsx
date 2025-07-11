53a0e12bc396356ef806acf92653fe0765483f06
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-07-11 16:27:42 +0300

    Frontend scaffolded with vite

---
+++ frontend/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div></div>
+      <h1>Aircraft Defect Viewer</h1>
+      <div className="card"></div>
+    </>
+  );
+}
+
+export default App;
