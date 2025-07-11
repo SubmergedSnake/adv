eae77c42942b7171cca78d4188ca4e34453327e5
Joonas Heikkinen <solarshadow.jh@gmail.com>
2025-07-12 00:35:33 +0300

    Get 100 most recent defects by default

--- backend/src/controllers/defectController.ts
+++ backend/src/controllers/defectController.ts
@@ -3,7 +3,10 @@ import { DefectCollection } from '../models/defectSchema';
 
 export const getDefects = async (req: Request, res: Response, next: NextFunction) => {
 	try {
-		const defects = await DefectCollection.find({});
+		const defects = await DefectCollection.find({})
+			.sort({ reported_at: -1 })
+			.limit(100)
+
 		res.json(defects)
 	}
 	catch (error) {
